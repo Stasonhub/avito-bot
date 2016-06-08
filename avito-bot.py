2c0ed88146a176341906160d9d37b2128efe9b5c
bjj <bjj@mac.local>
2016-06-08 22:50:56 +0300

    Checking is added. TODO: create a bot class

--- avito-bot.py
+++ avito-bot.py
@@ -1,5 +1,6 @@
 import sys, requests
 from bs4 import BeautifulSoup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 url = ''
 query = ''
@@ -18,14 +19,22 @@ class db:
     def __init__(self, parsed_page):
         self.parsed_page = parsed_page
         self.stored_list = {}
+        self.stored_list = self.create_item_list()
 
     def create_item_list(self):
         item_list = {}
         for div in self.parsed_page:
             local_item = item(div)
-            item_list[local_item.dict['id']] = local_item .dict
-        stored_list = item_list
+            item_list[local_item.dict['id']] = local_item.dict
         return item_list
+
+    def check_item_lists(self, new_list):
+        mybot = bot()
+        if self.stored_list.keys() != new_list.keys():
+            for key in new_list.keys():
+                if key not in self.stored_list.keys():
+                    self.stored_list = new_list
+                    return mybot.send_a_message(new_list[key])
 
 class item:
     def __init__(self, div):
@@ -35,35 +44,17 @@ class item:
         self.dict['title'] = div.find('a').get_text()
         self.dict['price'] = div.find('div').get_text()
         self.dict['description'] = div.find_all('p')
-    # def make_item_old():
-    #     self.item_new = false
 
-# def get_list():
-#     global query
-#     items_dict = {}
-#     query = query.upper()
-#     items = get_items(url)
-#     for item in items:
-#         title = item.find('a').get_text().upper()
-#         if title.find(query) != -1:
-#             items_dict[item.find('a').get('href')] = item
-#     return items_dict
+class bot:
+    def __init__(self):
+        print ('Hello')
 
-# def check_lists(old):
-#     global old_list
-#     new = get_list()
-#     if old.keys() != new.keys():
-#         print ('lists are not equal')
-#     else:
-#         print ('nothing new')
-
-# def send_message(message):
-#     print (message)
+    def send_a_message(self, message):
+        print (message)
 
 def main():
     global url
     global query
-    
     if len(sys.argv) >= 3:
         section = sys.argv[1]
         query = sys.argv[2]
@@ -74,16 +65,13 @@ if __name__ == '__main__':
     parsed_object = parser(url)
     parsed_object = parsed_object.get_parsed_page()
     db = db(parsed_object)
-    # db.create_item_list()
-    # old_list = get_list()
+    old_list = db.create_item_list()
+
+    #test 
+    db.stored_list.popitem()
+    new_list = db.create_item_list()
 
 # example: https://habrahabr.ru/post/302688/
-
-# class source:
-# class item:
-# class db:
-# class bot:
-
 
 # Q:
 # 1. global??
